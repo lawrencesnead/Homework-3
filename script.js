1b3108e7ef9df133b63ef05acda2eaf750bbc64b
Lawrence Snead <Lawrence.Snead@gmail.com>
2019-11-23 21:22:43 -0500

    Fixed all responsiveness related issues. Working on adding a shadow to the card element.

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+// Prompts for user to select what sort of password they would like to gnerate
+
 var passLength = prompt("How many characters would you like your password to be? Must be between 8 and 128.")
 var charTypeSpec = confirm("Would you like special characters?")
 var charTypeNum = confirm("Would you like numeric characters?")
 var charTypeLower = confirm("Would you like lowercase characters?")
 var charTypeUpper = confirm("Would you like uppercase characters?")
 
+// Varibles for buttons
+
 var generateBtn = document.querySelector("#gen")
-console.log(generateBtn)
 var copyBtn = document.querySelector("#copy")
 var passwordBox = document.querySelector("#pass-final")
+
+// Initial varibales with different sets of characters to select from
 
 var specialChars = " !\"/#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 var numChars = "0123456789"
@@ -15,6 +20,9 @@ var lowerChars = "abcdefghijklmnopqrstuvwxyz"
 var upperChars = lowerChars.toUpperCase()
 
 var charListFinal = ""
+
+// Helper function to make final character list to select from for password generation and then returns the completed list
+
 var generateFinalCharList = function () {
     if (charTypeSpec === true) {
         charListFinal += specialChars
@@ -31,7 +39,7 @@ var generateFinalCharList = function () {
     return charListFinal
 }
 
-
+// Function to generate a random password and place password into password div
 
 var generatePass = function () {
     // event.preventDefault()
@@ -43,6 +51,8 @@ var generatePass = function () {
     passwordBox.textContent = pass
 }
 
+// Copy generated password to the clipboard
+
 var copyText = function () {
     var copyText = document.getElementById("pass-final");
     var textArea = document.createElement("textarea");
@@ -50,8 +60,11 @@ var copyText = function () {
     document.body.appendChild(textArea);
     textArea.select();
     document.execCommand("Copy");
+    alert("\""+textArea.value+"\" has been copied to the clipboard!")
     textArea.remove();
    }
+
+// When buttons are clicked event listeners
 
 generateBtn.addEventListener("click", generatePass)
 copyBtn.addEventListener("click" , copyText)
